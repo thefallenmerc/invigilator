1841ee6a272cca32e7f043dfd21236781cb9895d
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-08-30 21:33:49 +0530

    added navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../pages/login-page';
+
+function NavbarComponent({ user }) {
+    return (
+        <div className="shadow px-2 py-3 bg-red-500 text-white flex justify-between">
+            <div>Invigilator</div>
+            {
+                user
+                    ? ''
+                    : (
+                        <div>
+                            <Link className="px-2 py-1 text-gray-300 hover:text-white" to={LOGIN_ROUTE}>Login</Link>
+                            <Link className="px-2 py-1 text-gray-300 hover:text-white" to={LOGIN_ROUTE}>Register</Link>
+                        </div>
+                    )
+
+            }
+        </div>
+    );
+}
+
+const mapStateToProps = state => ({
+    user: state.user
+});
+
+const Navbar = connect(mapStateToProps)(NavbarComponent);
+export default Navbar;
