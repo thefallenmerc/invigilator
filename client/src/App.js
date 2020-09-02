abc63081567a82bbb14646ffb90028df3083ef34
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-09-03 00:30:14 +0530

    added loading state visual to app moved provider from index.js to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,40 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import store from './redux'
-import { setStateSuccess } from './redux/actions/auth_actions'
-import { addUser } from './redux/actions/user_actions';
-import { Router, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { Router, Switch, Route, Link } from 'react-router-dom';
 import ProtectedRoute from './components/protected-route';
 import HomePage, { HOME_ROUTE } from './pages/home-page';
 import history from './config/history.config';
 import LoginPage, { LOGIN_ROUTE } from './pages/login-page';
 import Navbar from './components/navbar';
 import RegisterPage, { REGISTER_ROUTE } from './pages/register-page';
+import DashboardPage, { DASHBOARD_ROUTE } from './pages/dashboard-page';
+import { REQUEST_STATE_PENDING } from './redux/actions/auth_actions';
 
-const user = localStorage.getItem('user');
 
-if(user) {
-    store.dispatch(addUser(JSON.parse(user)));
-}
-
-export default function App() {
+function AppComponent({ state, user }) {
+    if (state === REQUEST_STATE_PENDING) {
+        return (
+            <div>Loading...</div>
+        );
+    }
     return (
-        <Provider store={store}>
-            <Router history={history}>
-                <Navbar />
-                <Switch>
-                    <Route path={HOME_ROUTE} exact={true} component={HomePage} />
-                    <Route path={LOGIN_ROUTE} component={LoginPage} />
-                    <Route path={REGISTER_ROUTE} component={RegisterPage} />
-                    <ProtectedRoute path="/dashboard" exact={true} component={HomePage} />
-                </Switch>
-            </Router>
-        </Provider>
+        <Router history={history}>
+            <Navbar />
+            <Switch>
+                <Route path={HOME_ROUTE} exact={true} component={HomePage} />
+                <Route path={LOGIN_ROUTE} component={LoginPage} />
+                <Route path={REGISTER_ROUTE} component={RegisterPage} />
+                <ProtectedRoute path={DASHBOARD_ROUTE} exact={true} user={user} component={DashboardPage} />
+            </Switch>
+        </Router>
     )
 }
+
+const mapStateToProps = state => ({
+    state: state.request_state,
+    user: state.user
+});
+
+const App = connect(mapStateToProps)(AppComponent);
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import store from './redux';
+import { Provider } from 'react-redux'
+import { addUser } from './redux/actions/user_actions';
+import { setStateSuccess } from './redux/actions/auth_actions';
+
 import App from "./App";
+
 import './styles/style.css';
 import './styles/main.css';
 
+const user = localStorage.getItem('user');
+
+if (user) {
+    store.dispatch(addUser(JSON.parse(user)));
+}
+store.dispatch(setStateSuccess());
 
 const wrapper = document.getElementById("app");
-wrapper ? ReactDOM.render(<App />, wrapper) : false;
+wrapper ? ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , wrapper) : false;
