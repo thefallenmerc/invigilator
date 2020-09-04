1945e630a21e5db521411741f3cbfc4dd9870af9
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-09-04 09:42:26 +0530

    added so so homepage

--- client/src/pages/home-page.js
+++ client/src/pages/home-page.js
@@ -4,8 +4,10 @@ export const HOME_ROUTE = "/";
 
 export default function HomePage() {
     return (
-        <div>
-            Home
+        <div className="flex justify-center items-center" style={{height: "calc(100vh - 48px)"}}>
+            <div className="text-xl uppercase text-red-500">
+                INVIGILATOR
+            </div>
         </div>
     )
 }
